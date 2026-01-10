c15f129a1ad6393df4d97358288a0f8664da69aa
kartikswamie29 <kartikswamie29@gmail.com>
2026-01-10 22:36:29 +0530

    Create SortArray.java

---
+++ SortArray.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        Arrays.sort(nums);
+    return nums;
+    }
+}
+
+
+
+ //     ArrayList<Integer> list = new ArrayList<>();
+    //     for(int num : nums)
+    //     {
+    //         list.add(num);
+    //     }
+    //     Collections.sort(list);
+
+    //      int[] arr= new int[list.size()];
+    //     int k=0;
+    //     for(int num : list)
+    //     {
+    //         arr[k] =num;
+    //         k++;
+    //     }        
+    // return arr;
